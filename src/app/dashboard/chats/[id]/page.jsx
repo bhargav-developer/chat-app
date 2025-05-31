99ed03cfb8a6d37c36755e4c396ec7ee1ef8e213
bhargav-developer <sujaybhargav4@gmail.com>
2025-05-31 23:53:24 +0530

    just adde a delete btn

--- src/app/dashboard/chats/[id]/page.jsx
+++ src/app/dashboard/chats/[id]/page.jsx
@@ -2,7 +2,7 @@
 import { useSocketStore } from '@/lib/socketStore'
 import { useUserStore } from '@/lib/userStore'
 import axios from 'axios'
-import { ArrowLeftCircleIcon, Link2Icon, SendHorizontalIcon, SmileIcon } from 'lucide-react'
+import { ArrowLeftCircleIcon, DeleteIcon, Link2Icon, SendHorizontalIcon, SmileIcon, Trash2Icon } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import * as React from 'react'
 
@@ -15,6 +15,16 @@ function page({ params }) {
   const router = useRouter();
   const socket = useSocketStore((state) => state.socket);
   const messagesEndRef = React.useRef(null);
+
+
+   const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleDeleteClick = () => setIsOpen(true);
+  const handleConfirmDelete = () => {
+    onDelete();
+    setIsOpen(false);
+  };
+  const handleCancel = () => setIsOpen(false);
 
   // Scroll effect
   React.useEffect(() => {
@@ -108,10 +118,10 @@ function page({ params }) {
             <p className="text-gray-600">online</p>
           </div>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-2 justify-between items-centers">
           <h1>op</h1>
           <h1>op</h1>
-          <h1>op</h1>
+        <Trash2Icon className='hover:text-indigo-500 cursor-pointer' onClick={handleDeleteClick} />
         </div>
       </div>
 
@@ -161,7 +171,35 @@ function page({ params }) {
           <SendHorizontalIcon className="text-white" />
         </button>
       </div>
+
+
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-md transition-opacity duration-300 z-50">
+          <div className="bg-white p-6 rounded shadow-md">
+            <h2 className="text-lg font-bold mb-4">Confirm Deletion</h2>
+            <p className="mb-4">Are you sure you want to delete this conversation?</p>
+            <div className="flex justify-end gap-4">
+              <button
+                onClick={handleCancel}
+                className="px-4 py-2 cursor-pointer bg-gray-300 rounded hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleConfirmDelete}
+                className="px-4 py-2 cursor-pointer bg-indigo-500 text-white rounded hover:bg-indigo-600"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+
+
+
+    
   );
 }
 
