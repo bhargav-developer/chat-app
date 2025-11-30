f2ae82bd7e6d020887c3f64338e19a5e646a2977
Bhargav <119781738+bhargav-developer@users.noreply.github.com>
2025-11-30 17:33:47 +0530

    Add icon to metadata in layout.tsx
    Added an icon URL to the metadata for the app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ import { Toaster } from "react-hot-toast";
 export const metadata: Metadata = {
   title: "Chat Sync",
   description: "Real-time chat and file sharing app",
+    icons: {
+    icon: "https://res.cloudinary.com/doql04ndg/image/upload/v1764501865/646f45f7-4d4c-4d62-99bd-2b5384ef22f2_cqdrbt.png",
+  }
 };
 
 export default function RootLayout({
